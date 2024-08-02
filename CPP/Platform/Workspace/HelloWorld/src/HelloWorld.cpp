1d93b208540cb5031fd1e66af6c1ee4e7d42b1cf
Stephen <mstephenlin@gmail.com>
2024-08-02 15:55:13 -0700

    feat: add cmakelists, include and hpp, and tests

--- CPP/Platform/Workspace/HelloWorld/src/HelloWorld.cpp
+++ CPP/Platform/Workspace/HelloWorld/src/HelloWorld.cpp
@@ -1,17 +1,22 @@
+
+#include "HelloWorld.hpp"
 #include <iostream>
-#include <Eigen/Dense>
-
 using namespace std;
-using namespace Eigen;
-int main(int argc, char** argv)
-{
-    cout << "Hello World \n";
 
-    Matrix2d mat;
-    mat(0, 0) = 3;
-    mat(1, 0) = 2.5;
-    mat(0, 1) = -1;
-    mat(1, 1) = mat(1, 0) + mat(0, 1);
-    cout << mat << endl;
-    return 0;
+HelloWorld::HelloWorld()
+{
+}
+
+HelloWorld::~HelloWorld()
+{
+}   
+
+void HelloWorld::inEnglish()
+{
+    cout << "Hello World" << endl;
+}
+
+void HelloWorld::inSpanish()
+{
+    cout << "Hola Mundo" << endl;
 }
