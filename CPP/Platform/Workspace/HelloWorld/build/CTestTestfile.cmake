f6a07a0270bd76a1fd58f9dff1746052315ad652
Stephen <mstephenlin@gmail.com>
2024-08-02 15:48:21 -0700

    WIP: debugging build steps

---
+++ CPP/Platform/Workspace/HelloWorld/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/Developer/MIG-Bot/CPP/Platform/Workspace/HelloWorld
+# Build directory: /home/parallels/Developer/MIG-Bot/CPP/Platform/Workspace/HelloWorld/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[HelloWorldTest]=] "/home/parallels/Developer/MIG-Bot/CPP/Platform/Workspace/HelloWorld/build/HelloWorldTest")
+set_tests_properties([=[HelloWorldTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/parallels/Developer/MIG-Bot/CPP/Platform/Workspace/HelloWorld/CMakeLists.txt;39;add_test;/home/parallels/Developer/MIG-Bot/CPP/Platform/Workspace/HelloWorld/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
